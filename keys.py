b2b10e3bf1139c6946dd03267b95d94c99cfc4bf
Revant Kumar Gupta <125722896+revantkumargupta@users.noreply.github.com>
2023-04-20 06:46:47 +0545

    Add files via upload

---
+++ keys.py
@@ -0,0 +1,15 @@
+import os 
+
+api_id = os.getenv('api_id') 
+api_hash = os.getenv('api_hash') 
+bot_token = os.getenv('bot_token') 
+
+service_file = os.getenv('service_file') 
+db_url = os.getenv('db_url') 
+deezer_api = os.getenv('deezer_api') 
+
+team_drive_id = os.getenv('team_drive_id') 
+folder_id = os.getenv('folder_id') 
+index_link = os.getenv('index_link')
+
+service_file_url = os.getenv('service_file_url')
